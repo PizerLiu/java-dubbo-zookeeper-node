8c4268b92f2be391478838f737bf64edc15dbc4c
pizer <438732659@qq.com>
2017-11-02 15:21:28 +0800

    no message

--- node-zookeeper-dubbo-java/controllers/pizerRestful.js
+++ node-zookeeper-dubbo-java/controllers/pizerRestful.js
@@ -4,7 +4,7 @@ const responseUtils = require('../common/responseUtils');
 
 module.exports = {
     'POST /api/pizer': async (ctx) => {
-        let response = await pizerDao.pizer.OutString(ctx.response.body);
+        let response = await pizerDao.pizer.OutString(ctx.request.body);
 
         responseUtils.success(ctx.response, response);
     }
